c03798695378450b5d79004c968efc6169aed014
solo_cc <15201591877@139.com>
2020-07-02 16:56:14 +0800

    01构建项目路由加载Element

---
+++ vue-admin/src/views/login/index.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="login">
+        <h1>index page</h1>
+    </div>
+</template>
+<style lang="scss" scoped>
+    #login{
+        height: 100vh;
+        background-color: #bebee0;
+    }
+</style>
+<script>
+export default {
+    
+}
+</script>
